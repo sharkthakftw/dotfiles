e03471cfde738525d6f328781acb4a607af19d32
sharkthakftw <sharkthakftw@gmail.com>
2025-07-04 19:34:59 +0530

    waybar cava script

---
+++ .config/waybar/scripts/waybar-cava.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+bar="▁▂▃▄▅▆▇█"
+dict="s/;//g"
+
+bar_length=${#bar}
+
+for ((i = 0; i < bar_length; i++)); do
+    dict+=";s/$i/${bar:$i:1}/g"
+done
+
+config_file="/tmp/bar_cava_config"
+cat >"$config_file" <<EOF
+[general]
+bars = 12
+
+[input]
+method = pulse
+source = auto
+
+[output]
+method = raw
+raw_target = /dev/stdout
+data_format = ascii
+ascii_max_range = 7
+EOF
+
+pkill -f "cava -p $config_file"
+
+cava -p "$config_file" | sed -u "$dict"
