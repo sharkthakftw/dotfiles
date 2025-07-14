abe59129a2985342dafe910df31506a35f65b923
sharkthakftw <sharkthakftw@gmail.com>
2025-07-15 03:21:59 +0530

    switched to vesktop
    Using vesktop
    
    ?

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -53,7 +53,7 @@ if type -q keychain
 end
 
 # Yazi
-function y
+function yz
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
     yazi $argv --cwd-file="$tmp"
     if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
